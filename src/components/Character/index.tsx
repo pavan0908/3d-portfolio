af4767ba75363a144f13eb9e5bb5c495e7056a39
pavan0908 <138233228+pavan0908@users.noreply.github.com>
2026-06-05 11:37:45 -0500

    Replace 3D Character with static image - remove Three.js dependency
    Removed camera initialization and adjusted component styles.

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,12 +1,9 @@
 import { useEffect } from "react";
 import { setCharTimeline, setAllTimeline } from "../utils/GsapScroll";
-import * as THREE from "three";
 
 const CharacterModel = () => {
   useEffect(() => {
-    const camera = new THREE.PerspectiveCamera(14.5, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 13.1, 24.7);
-    setCharTimeline(null, camera);
+    setCharTimeline(null, null);
     setAllTimeline();
   }, []);
 
@@ -14,16 +11,11 @@ const CharacterModel = () => {
     <div
       className="character-model"
       style={{
-        position: "absolute",
-        right: "5%",
-        bottom: "0",
-        width: "460px",
-        maxWidth: "45vw",
-        zIndex: 2,
-        pointerEvents: "none",
         display: "flex",
         alignItems: "flex-end",
         justifyContent: "center",
+        width: "100%",
+        height: "100%",
       }}
     >
       <img
