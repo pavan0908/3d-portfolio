bd36ec07185c229255c312d05e611b6e1e4ab850
pavan0908 <138233228+pavan0908@users.noreply.github.com>
2026-06-06 19:45:08 -0500

    Fix avatar on desktop: always pass children into Landing section

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -36,11 +36,10 @@ const MainContainer = ({ children }: PropsWithChildren) => {
       <Cursor />
       <Navbar />
       <SocialIcons />
-      {isDesktopView && children}
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <div className="container-main">
-            <Landing>{!isDesktopView && children}</Landing>
+            <Landing>{children}</Landing>
             <About />
             <WhatIDo />
             <Skills />
